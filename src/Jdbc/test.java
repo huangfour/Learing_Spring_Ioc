476ed41a3e67d13759384c461f2a80eab030a523
four <294863053@qq.com>
2020-04-01 18:47:02 +0800

    简单案例

---
+++ src/Jdbc/test.java
@@ -0,0 +1,19 @@
+package Jdbc;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @author : hong.Four
+ * @date : 2020-04-01 18:15
+ **/
+public class test {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("Jdbc/Jdbc.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        System.out.println("JdbcTemplate测试");
+
+        System.out.println( jdbcTemplate.queryForList("SELECT * FROM `items` where root_cat_id='2'"));
+    }
+
+}
